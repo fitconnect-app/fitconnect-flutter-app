23013236535658ef3fa5464c3148926639ad5c4c
Juan Alegría <j.alegria@uniandes.edu.co>
2023-03-14 18:16:55 -0500

    Feat: init routing and screens file structure

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,15 +1,14 @@
 import 'package:flutter/widgets.dart';
-import 'package:mas_colombia_app/screens/account/account.dart';
-import 'package:mas_colombia_app/screens/events/events.dart';
-import 'package:mas_colombia_app/screens/login/login.dart';
-import 'package:mas_colombia_app/screens/signup/signup.dart';
-import 'package:mas_colombia_app/screens/start/start.dart';
+// import 'package:fit_connect/screens/account/account.dart';
+// import 'package:fit_connect/screens/events/events.dart';
+// import 'package:fit_connect/screens/login/login.dart';
+// import 'package:fit_connect/screens/signup/signup.dart';
+// import 'package:fit_connect/screens/start/start.dart';
 
 final Map<String, WidgetBuilder> routes = <String, WidgetBuilder>{
-  "/start": (BuildContext context) => const StartScreen(),
-  "/signup": (BuildContext context) => const SignUpScreen(),
-  "/login": (BuildContext context) => const LoginScreen(),
-  "/events": (BuildContext context) => const EventsScreen(),
-  "/account": (BuildContext context) => const AccountScreen(),
+  // TODO: "/signup": (BuildContext context) => const SignUpScreen(),
+  // TODO: "/login": (BuildContext context) => const LoginScreen(),
+  // TODO: "/events": (BuildContext context) => const EventsScreen(),
+  // TODO: "/account": (BuildContext context) => const AccountScreen(),
   // TODO: "/account-edit": (BuildContext context) => AccountEditScreen(),
 };
