c2e01bda4f05d5aa7cade4ac8463195a8a90f225
Juan Alegría <j.alegria@uniandes.edu.co>
2023-03-14 19:23:04 -0500

    Feat: add signup screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,17 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/material.dart';
 import 'package:fit_connect/routes.dart';
 import 'package:fit_connect/theme/style.dart';
+import 'package:flutter/material.dart';
 
 import 'firebase_options.dart';
 
 Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   User? user = FirebaseAuth.instance.currentUser;
-  String initialRoute = user == null ? '/signup' : '/events';
+  String initialRoute = '/signup';
+  // TODO: String initialRoute = user == null ? '/signup' : '/events';
 
   // Run app
   runApp(MyApp(initialRoute: initialRoute));
--- lib/routes.dart
+++ lib/routes.dart
@@ -1,12 +1,13 @@
+import 'package:fit_connect/screens/signup/signup.dart';
 import 'package:flutter/widgets.dart';
+
 // import 'package:fit_connect/screens/account/account.dart';
 // import 'package:fit_connect/screens/events/events.dart';
 // import 'package:fit_connect/screens/login/login.dart';
-// import 'package:fit_connect/screens/signup/signup.dart';
 // import 'package:fit_connect/screens/start/start.dart';
 
 final Map<String, WidgetBuilder> routes = <String, WidgetBuilder>{
-  // TODO: "/signup": (BuildContext context) => const SignUpScreen(),
+  "/signup": (BuildContext context) => const SignUpScreen(),
   // TODO: "/login": (BuildContext context) => const LoginScreen(),
   // TODO: "/events": (BuildContext context) => const EventsScreen(),
   // TODO: "/account": (BuildContext context) => const AccountScreen(),
