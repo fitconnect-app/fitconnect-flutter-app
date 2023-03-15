1992aa5b95b472365c1f9d18c5e86b4bffbbeebd
Juan Alegría <j.alegria@uniandes.edu.co>
2023-03-15 15:55:02 -0500

    Style: format code

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:firebase_auth/firebase_auth.dart';
+// import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:fit_connect/routes.dart';
 import 'package:fit_connect/theme/style.dart';
@@ -9,7 +9,7 @@ import 'firebase_options.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  User? user = FirebaseAuth.instance.currentUser;
+  // User? user = FirebaseAuth.instance.currentUser;
   String initialRoute = '/auth';
   // TODO: String initialRoute = user == null ? '/signup' : '/home';
 
