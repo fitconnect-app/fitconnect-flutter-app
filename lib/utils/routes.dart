94d348465b89092f6fe139dc29bef278946f98e1
ElReyZero <juan.romero1201@gmail.com>
2023-03-22 14:16:11 -0500

    Feat: Add bar plots to stat

--- lib/utils/routes.dart
+++ lib/utils/routes.dart
@@ -2,6 +2,7 @@ import 'package:fit_connect/screens/auth/auth_screen.dart';
 import 'package:fit_connect/screens/bpm/bpm_screen.dart';
 import 'package:fit_connect/screens/home/home_screen.dart';
 import 'package:fit_connect/screens/stats/stats_screen.dart';
+import 'package:fit_connect/screens/stats/stats_screen.dart';
 import 'package:flutter/widgets.dart';
 
 // import 'package:fit_connect/screens/account/account.dart';
@@ -12,7 +13,7 @@ final Map<String, WidgetBuilder> routes = <String, WidgetBuilder>{
   "/auth": (BuildContext context) => const AuthScreen(),
   "/home": (BuildContext context) => const HomeScreen(),
   "/bpm": (BuildContext context) => const BPMScreen(),
-  "/stats": (BuildContext context) => const PersonalStatsScreen(),
+  "/stats": (BuildContext context) => const StatsScreen(),
   // TODO: "/events": (BuildContext context) => const EventsScreen(),
   // TODO: "/account": (BuildContext context) => const AccountScreen(),
   // TODO: "/account-edit": (BuildContext context) => AccountEditScreen(),
--- lib/view_model/stats_view_model.dart
+++ lib/view_model/stats_view_model.dart
@@ -1,97 +1,47 @@
-// Import the models
-import 'package:fit_connect/model/shared/sports.dart';
+import 'dart:math';
+import 'package:fit_connect/theme/style.dart';
 import 'package:flutter/material.dart';
 
-class PersonalStatsViewModel extends ChangeNotifier {
-  // Define the data sources
-  final List<String> _sports = Sports.values.map((e) => e.getString()).toList();
+class Data {
+  final int id;
+  final String label;
+  final double yValue;
+  final Color color;
 
-  // Define the state variables
-  bool _isLoading = true;
-  List<SportCount> _mostPlayedSports = [];
-  List<HourCount> _mostFrequentHours = [];
-  List<SportHours> _practicedSportHours = [];
+  Data({required this.id, required this.label, required this.yValue, required this.color});
+}
 
-  // Define the getters for the state variables
-  bool get isLoading => _isLoading;
-  List<SportCount> get mostPlayedSports => _mostPlayedSports;
-  List<HourCount> get mostFrequentHours => _mostFrequentHours;
-  List<SportHours> get practicedSportHours => _practicedSportHours;
+class MyPersonalStatisticsViewModel extends ChangeNotifier {
+  List<Data> mostSearchedSports = [];
+  List<Data> mostFrequentHours = [];
+  List<Data> hoursPracticed = [];
 
-  // Define the constructor
-  PersonalStatsViewModel() {
-    // Simulate loading the data from a database or API
-    Future.delayed(const Duration(seconds: 2), () {
-      _generateData();
-      _isLoading = false;
-      notifyListeners();
-    });
+  MyPersonalStatisticsViewModel() {
+    generateRandomData();
   }
 
-  // Define the method that generates the data
-  void _generateData() {
-    // Generate random data for the most played sports
-    _mostPlayedSports = List.generate(
-      4,
-      (index) => SportCount(
-        name: _sports[index],
-        count: 100 + index * 50,
-        icon: Sports.values[index].getIcon(),
-      ),
-    );
+  void generateRandomData() {
+    final random = Random();
+    final colors = [lightColorScheme.primary, lightColorScheme.secondary, lightColorScheme.tertiary];
+    mostSearchedSports = List.generate(7, (index) => Data(
+      id: index,
+      label: 'Sport ${index + 1}',
+      yValue: random.nextInt(10).toDouble(),
+      color: colors[random.nextInt(colors.length)],
+    ));
+    mostFrequentHours = List.generate(12, (index) => Data(
+      id: index,
+      label: '${index + 1}h',
+      yValue: random.nextInt(10).toDouble(),
+      color: colors[random.nextInt(colors.length)],
+    ));
+    hoursPracticed = List.generate(7, (index) => Data(
+      id: index,
+      label: 'Sport ${index + 1}',
+      yValue: random.nextInt(10).toDouble(),
+      color: colors[random.nextInt(colors.length)],
+    ));
 
-    // Generate random data for the most frequent hours
-    _mostFrequentHours = List.generate(
-      4,
-      (index) => HourCount(
-        hour: index * 6,
-        count: 50 + index * 25
-      ),
-    );
-
-    // Generate random data for the practiced sport hours
-    _practicedSportHours = List.generate(
-      4,
-      (index) => SportHours(
-        name: _sports[index],
-        hours: 5 + index * 2,
-        icon: Sports.values[index].getIcon(),
-      ),
-    );
+    notifyListeners();
   }
-}
-
-
-class SportCount {
-  final String? name;
-  final int? count;
-  final IconData? icon;
-
-  SportCount({
-    @required this.name,
-    @required this.count,
-    @required this.icon,
-  });
-}
-
-class HourCount {
-  final int? hour;
-  final int? count;
-
-  HourCount({
-    @required this.hour,
-    @required this.count,
-  });
-}
-
-class SportHours {
-  final String? name;
-  final int? hours;
-  final IconData? icon;
-
-  SportHours({
-    @required this.name,
-    @required this.hours,
-    @required this.icon,
-  });
 }
