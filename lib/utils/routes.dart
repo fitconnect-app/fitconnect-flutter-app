7d0ea4196e2dfffdecf09a510d3a06066e2232ed
ElReyZero <juan.romero1201@gmail.com>
2023-03-23 22:31:13 -0500

    Fix: Remove duplicate import, unused todos

--- lib/utils/routes.dart
+++ lib/utils/routes.dart
@@ -3,12 +3,8 @@ import 'package:fit_connect/screens/auth/auth_screen.dart';
 import 'package:fit_connect/screens/bpm/bpm_screen.dart';
 import 'package:fit_connect/screens/home/home_screen.dart';
 import 'package:fit_connect/screens/stats/stats_screen.dart';
-import 'package:fit_connect/screens/stats/stats_screen.dart';
 import 'package:flutter/widgets.dart';
 
-// import 'package:fit_connect/screens/account/account.dart';
-// import 'package:fit_connect/screens/events/events.dart';
-// import 'package:fit_connect/screens/start/start.dart';
 
 final Map<String, WidgetBuilder> routes = <String, WidgetBuilder>{
   "/auth": (BuildContext context) => const AuthScreen(),
@@ -17,6 +13,5 @@ final Map<String, WidgetBuilder> routes = <String, WidgetBuilder>{
   "/bpm": (BuildContext context) => const BPMScreen(),
   "/stats": (BuildContext context) => const StatsScreen(),
   // TODO: "/events": (BuildContext context) => const EventsScreen(),
-  // TODO: "/account": (BuildContext context) => const AccountScreen(),
-  // TODO: "/account-edit": (BuildContext context) => AccountEditScreen(),
+  // TODO: "/profile-edit": (BuildContext context) => ProfileEditScreen(),
 };
