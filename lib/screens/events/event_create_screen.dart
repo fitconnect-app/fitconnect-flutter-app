b2373a8c619b2c0eaa0146b073dd5923cc8701a9
Luis Castro <ld.castro@uniandes.edu.co>
2023-03-24 19:18:17 -0500

    Fix: Remove Overflow createevent view

--- lib/screens/events/event_create_screen.dart
+++ lib/screens/events/event_create_screen.dart
@@ -43,202 +43,206 @@ class SportFormState extends State<SportFormScreen> {
         title: const Text('Sport Form'),
         centerTitle: true,
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Form(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const Text(
-                'Choose the sport',
-                style: TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.black,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Form(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text(
+                  'Choose the sport',
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                  ),
                 ),
-              ),
-              const SizedBox(height: 5),
-              DropdownButton<String>(
-                isExpanded: true,
-                value: _selectedSport,
-                hint: const Text('Select a sport'),
-                onChanged: (String? newValue) {
-                  setState(() {
-                    _selectedSport = newValue;
-                  });
-                },
-                items: _sports.map<DropdownMenuItem<String>>((String value) {
-                  return DropdownMenuItem<String>(
-                    value: value,
-                    child: Text(value),
-                  );
-                }).toList(),
-              ),
-              const SizedBox(height: 15),
-              const Text(
-                'How many players are needed?',
-                style: TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.black,
+                const SizedBox(height: 5),
+                DropdownButton<String>(
+                  isExpanded: true,
+                  value: _selectedSport,
+                  hint: const Text('Select a sport'),
+                  onChanged: (String? newValue) {
+                    setState(() {
+                      _selectedSport = newValue;
+                    });
+                  },
+                  items: _sports.map<DropdownMenuItem<String>>((String value) {
+                    return DropdownMenuItem<String>(
+                      value: value,
+                      child: Text(value),
+                    );
+                  }).toList(),
                 ),
-              ),
-              const SizedBox(height: 5),
-              DropdownButton<int>(
-                isExpanded: true,
-                value: _selectedPlayerCount,
-                hint: const Text('Select a number of players'),
-                onChanged: (int? newValue) {
-                  setState(() {
-                    _selectedPlayerCount = newValue;
-                  });
-                },
-                items: _playerCounts.map<DropdownMenuItem<int>>((int value) {
-                  return DropdownMenuItem<int>(
-                    value: value,
-                    child: Text(value.toString()),
-                  );
-                }).toList(),
-              ),
-              const SizedBox(height: 15),
-              const Text(
-                'How many players are you bringing?',
-                style: TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.black,
+                const SizedBox(height: 15),
+                const Text(
+                  'How many players are needed?',
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                  ),
                 ),
-              ),
-              const SizedBox(height: 5),
-              DropdownButton<int>(
-                isExpanded: true,
-                value:
-                    _broughtPlayerCount, // Actualizado a una segunda variable
-                hint: const Text('Select the number of players'),
-                onChanged: (int? newValue) {
-                  setState(() {
-                    _broughtPlayerCount =
-                        newValue; // Actualizado a una segunda variable
-                  });
-                },
-                items: _playerCounts.map<DropdownMenuItem<int>>((int value) {
-                  return DropdownMenuItem<int>(
-                    value: value,
-                    child: Text(value.toString()),
-                  );
-                }).toList(),
-              ),
-              const SizedBox(height: 15),
-              const Text(
-                'Select the event\'s date and time',
-                style: TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.black,
+                const SizedBox(height: 5),
+                DropdownButton<int>(
+                  isExpanded: true,
+                  value: _selectedPlayerCount,
+                  hint: const Text('Select a number of players'),
+                  onChanged: (int? newValue) {
+                    setState(() {
+                      _selectedPlayerCount = newValue;
+                    });
+                  },
+                  items: _playerCounts.map<DropdownMenuItem<int>>((int value) {
+                    return DropdownMenuItem<int>(
+                      value: value,
+                      child: Text(value.toString()),
+                    );
+                  }).toList(),
                 ),
-              ),
-              const SizedBox(height: 5),
-              Row(
-                children: [
-                  Expanded(
+                const SizedBox(height: 15),
+                const Text(
+                  'How many players are you bringing?',
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                  ),
+                ),
+                const SizedBox(height: 5),
+                DropdownButton<int>(
+                  isExpanded: true,
+                  value:
+                      _broughtPlayerCount, // Actualizado a una segunda variable
+                  hint: const Text('Select the number of players'),
+                  onChanged: (int? newValue) {
+                    setState(() {
+                      _broughtPlayerCount =
+                          newValue; // Actualizado a una segunda variable
+                    });
+                  },
+                  items: _playerCounts.map<DropdownMenuItem<int>>((int value) {
+                    return DropdownMenuItem<int>(
+                      value: value,
+                      child: Text(value.toString()),
+                    );
+                  }).toList(),
+                ),
+                const SizedBox(height: 15),
+                const Text(
+                  'Select the event\'s date and time',
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                  ),
+                ),
+                const SizedBox(height: 5),
+                Row(
+                  children: [
+                    Expanded(
+                      child: TextField(
+                        controller: _dateController,
+                        readOnly: true,
+                        decoration: const InputDecoration(
+                          hintText: 'Select date and time',
+                        ),
+                      ),
+                    ),
+                    IconButton(
+                      onPressed: () async {
+                        final DateTime? pickedDate = await showDatePicker(
+                          context: context,
+                          initialDate: DateTime.now(),
+                          firstDate: DateTime.now(),
+                          lastDate:
+                              DateTime.now().add(const Duration(days: 365)),
+                        );
+
+                        if (pickedDate != null) {
+                          final TimeOfDay? pickedTime = await showTimePicker(
+                            context: context,
+                            initialTime: TimeOfDay.now(),
+                          );
+
+                          if (pickedTime != null) {
+                            setState(() {
+                              _selectedDateTime = DateTime(
+                                pickedDate.year,
+                                pickedDate.month,
+                                pickedDate.day,
+                                pickedTime.hour,
+                                pickedTime.minute,
+                              );
+                            });
+
+                            _dateController.text =
+                                DateFormat('yyyy-MM-dd HH:mm')
+                                    .format(_selectedDateTime!);
+
+                            print(
+                                'Selected date and time: ${DateFormat('yyyy-MM-dd HH:mm').format(_selectedDateTime!)}');
+                          }
+                        }
+                      },
+                      icon: const Icon(Icons.calendar_today),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 15),
+                const Text(
+                  'Duration',
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                  ),
+                ),
+                const SizedBox(height: 5),
+                GestureDetector(
+                  onTap: () async {
+                    Duration? pickedDuration = await showDurationPicker(
+                      context: context,
+                      initialTime: const Duration(),
+                    );
+
+                    if (pickedDuration != null) {
+                      setState(() {
+                        _duration = pickedDuration;
+                        _durationController.text =
+                            '${pickedDuration.inHours.toString().padLeft(2, '0')}:${(pickedDuration.inMinutes % 60).toString().padLeft(2, '0')}';
+                      });
+                    }
+                  },
+                  child: AbsorbPointer(
                     child: TextField(
-                      controller: _dateController,
-                      readOnly: true,
+                      controller: _durationController,
                       decoration: const InputDecoration(
-                        hintText: 'Select date and time',
+                        hintText: 'HH:mm',
                       ),
                     ),
                   ),
-                  IconButton(
-                    onPressed: () async {
-                      final DateTime? pickedDate = await showDatePicker(
-                        context: context,
-                        initialDate: DateTime.now(),
-                        firstDate: DateTime.now(),
-                        lastDate: DateTime.now().add(const Duration(days: 365)),
-                      );
-
-                      if (pickedDate != null) {
-                        final TimeOfDay? pickedTime = await showTimePicker(
-                          context: context,
-                          initialTime: TimeOfDay.now(),
-                        );
-
-                        if (pickedTime != null) {
-                          setState(() {
-                            _selectedDateTime = DateTime(
-                              pickedDate.year,
-                              pickedDate.month,
-                              pickedDate.day,
-                              pickedTime.hour,
-                              pickedTime.minute,
-                            );
-                          });
-
-                          _dateController.text = DateFormat('yyyy-MM-dd HH:mm')
-                              .format(_selectedDateTime!);
-
-                          print(
-                              'Selected date and time: ${DateFormat('yyyy-MM-dd HH:mm').format(_selectedDateTime!)}');
-                        }
-                      }
-                    },
-                    icon: const Icon(Icons.calendar_today),
-                  ),
-                ],
-              ),
-              const SizedBox(height: 15),
-              const Text(
-                'Duration',
-                style: TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.black,
                 ),
-              ),
-              const SizedBox(height: 5),
-              GestureDetector(
-                onTap: () async {
-                  Duration? pickedDuration = await showDurationPicker(
-                    context: context,
-                    initialTime: const Duration(),
-                  );
-
-                  if (pickedDuration != null) {
-                    setState(() {
-                      _duration = pickedDuration;
-                      _durationController.text =
-                          '${pickedDuration.inHours.toString().padLeft(2, '0')}:${(pickedDuration.inMinutes % 60).toString().padLeft(2, '0')}';
-                    });
-                  }
-                },
-                child: AbsorbPointer(
-                  child: TextField(
-                    controller: _durationController,
-                    decoration: const InputDecoration(
-                      hintText: 'HH:mm',
-                    ),
+                const SizedBox(height: 15),
+                const Text(
+                  'Event Location',
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
                   ),
                 ),
-              ),
-              const SizedBox(height: 15),
-              const Text(
-                'Event Location',
-                style: TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.black,
+                const SizedBox(height: 5),
+                TextField(
+                  decoration: const InputDecoration(
+                    hintText: 'Enter event location',
+                  ),
+                  controller: _locationController,
                 ),
-              ),
-              const SizedBox(height: 5),
-              TextField(
-                decoration: const InputDecoration(
-                  hintText: 'Enter event location',
-                ),
-                controller: _locationController,
-              ),
-              const SizedBox(height: 15),
-            ],
+                const SizedBox(height: 15),
+              ],
+            ),
           ),
         ),
       ),
