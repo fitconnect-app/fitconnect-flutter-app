6c41347119d2257c4bf1c987d27479f5efb00740
Juan Alegría <j.alegria@uniandes.edu.co>
2023-05-24 06:52:21 -0500

    Feat: create emergency model

---
+++ lib/model/emergency/emergency_model.dart
@@ -0,0 +1,19 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class EmergencyModel {
+  String id;
+  String userName;
+  String location;
+  String reason;
+  Timestamp timestamp;
+  String status;
+
+  EmergencyModel({
+    required this.id,
+    required this.userName,
+    required this.location,
+    required this.reason,
+    required this.timestamp,
+    required this.status,
+  });
+}
