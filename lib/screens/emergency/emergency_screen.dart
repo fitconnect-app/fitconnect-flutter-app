155238a79a18d6a9539e7675080527ef68ba5d76
Juan Alegría <j.alegria@uniandes.edu.co>
2023-05-24 17:48:00 -0500

    Feat: add loading indicator to emergency screen

--- lib/screens/emergency/emergency_screen.dart
+++ lib/screens/emergency/emergency_screen.dart
@@ -17,13 +17,13 @@ class _EmergencyScreenState extends State<EmergencyScreen> {
   late EmergencyViewModel _viewModel;
 
   @override
-  void initState() {
-    super.initState();
+  void dispose() {
+    _viewModel.closeEmergencyStream();
+    super.dispose();
   }
 
   void _sendHelpRequest() async {
-    _viewModel.sendHelpRequest(_viewModel.reason);
-    _viewModel.changeEmergencyState(EmergencyState.isWaiting);
+    await _viewModel.sendHelpRequest();
   }
 
   @override
@@ -36,191 +36,200 @@ class _EmergencyScreenState extends State<EmergencyScreen> {
     return ChangeNotifierProvider<EmergencyViewModel>(
       create: (context) => _viewModel,
       child: Consumer<EmergencyViewModel>(builder: (context, viewModel, child) {
-        return Scaffold(
-          appBar: AppBar(
-            title: const Text('Emergency'),
-          ),
-          body: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                if (viewModel.state == EmergencyState.isInitialized)
-                  Expanded(
-                    child: Column(
-                      children: [
-                        const Padding(
-                          padding: EdgeInsets.symmetric(
-                              horizontal: 50, vertical: 30),
-                          child: Column(
-                            children: [
-                              Text(
-                                'Please read the instructions below before requesting help',
-                                textAlign: TextAlign.center,
-                                style: TextStyle(
-                                  fontSize: 18,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                              SizedBox(height: 20),
-                              Text(
-                                'IMPORTANT: This emergency section should only be used in genuine emergency situations.',
-                                textAlign: TextAlign.center,
-                                style: TextStyle(
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.red,
-                                ),
-                              ),
-                              SizedBox(height: 20),
-                              Text(
-                                'Please remember to activate your GPS to allow us to locate you easier',
-                                textAlign: TextAlign.center,
-                                style: TextStyle(
-                                  fontSize: 16,
-                                  color: Colors.black,
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                        SizedBox(
-                          width: 200,
-                          child: DotLottieLoader.fromAsset(
-                            'assets/animations/alert.lottie',
-                            frameBuilder:
-                                (BuildContext ctx, DotLottie? dotlottie) {
-                              if (dotlottie != null) {
-                                return Lottie.memory(
-                                    dotlottie.animations.values.single);
-                              } else {
-                                return Container();
-                              }
-                            },
-                          ),
-                        ),
-                        const SizedBox(height: 30),
-                        Container(
-                          constraints: const BoxConstraints(maxWidth: 300),
-                          child: DropdownButtonFormField<String>(
-                            decoration: const InputDecoration(
-                              labelText: 'Reason',
-                              border: OutlineInputBorder(),
-                            ),
-                            items: const [
-                              DropdownMenuItem(
-                                value: 'General Accident',
-                                child: Text('General Accident'),
-                              ),
-                              DropdownMenuItem(
-                                value: 'Medical Emergency',
-                                child: Text('Medical Emergency'),
-                              ),
-                              DropdownMenuItem(
-                                value: 'Natural Disaster',
-                                child: Text('Natural Disaster'),
-                              ),
-                              DropdownMenuItem(
-                                value: 'Facility Damage',
-                                child: Text('Facility Damage'),
-                              ),
-                            ],
-                            onChanged: (String? value) {
-                              viewModel.setReason(value);
-                            },
-                          ),
-                        ),
-                        const SizedBox(height: 40),
-                        SliderButton(
-                          action: _sendHelpRequest,
-                          label: const Text(
-                            'Slide to request help',
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontWeight: FontWeight.bold,
-                              fontSize: 14,
-                            ),
-                          ),
-                          icon: const Icon(
-                            Icons.keyboard_double_arrow_right,
-                            color: Colors.white,
-                            size: 50,
-                          ),
-                          buttonColor: lightColorScheme.tertiary.withOpacity(1),
-                          backgroundColor:
-                              lightColorScheme.tertiary.withOpacity(0.4),
-                          baseColor:
-                              lightColorScheme.secondary.withOpacity(0.7),
-                        ),
-                      ],
-                    ),
-                  ),
-                if (viewModel.state == EmergencyState.isWaiting)
-                  Expanded(
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 60, vertical: 60),
-                      child: Column(
-                        children: [
-                          const Text(
-                            'Please wait until an administrator can be reached',
-                            textAlign: TextAlign.center,
-                            style: TextStyle(fontSize: 20),
-                          ),
-                          const SizedBox(height: 130),
-                          Transform.scale(
-                            scale: 2,
-                            child: DotLottieLoader.fromAsset(
-                                'assets/animations/rest-wait.lottie',
-                                frameBuilder:
-                                    (BuildContext ctx, DotLottie? dotlottie) {
-                              if (dotlottie != null) {
-                                return Lottie.memory(
-                                    dotlottie.animations.values.single);
-                              } else {
-                                return Container();
-                              }
-                            }),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                if (viewModel.state == EmergencyState.isAdminApproved)
-                  Expanded(
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 60, vertical: 60),
-                      child: Column(
-                        children: [
-                          const Text(
-                            'Congratulations, an administrator has approved your request and it\'s on its way',
-                            textAlign: TextAlign.center,
-                            style: TextStyle(fontSize: 20),
-                          ),
-                          const SizedBox(height: 100),
-                          Transform.scale(
-                            scale: 1.5,
-                            child: DotLottieLoader.fromAsset(
-                                'assets/animations/meditation-wait-please.lottie',
-                                frameBuilder:
-                                    (BuildContext ctx, DotLottie? dotlottie) {
-                              if (dotlottie != null) {
-                                return Lottie.memory(
-                                  dotlottie.animations.values.single,
-                                );
-                              } else {
-                                return Container();
-                              }
-                            }),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-              ],
+        if (viewModel.state == EmergencyState.isLoading) {
+          return const Scaffold(
+            body: Center(
+              child: CircularProgressIndicator(),
             ),
-          ),
-        );
+          );
+        } else {
+          return Scaffold(
+            appBar: AppBar(
+              title: const Text('Emergency'),
+            ),
+            body: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  if (viewModel.state == EmergencyState.isInitialized)
+                    Expanded(
+                      child: Column(
+                        children: [
+                          const Padding(
+                            padding: EdgeInsets.symmetric(
+                                horizontal: 50, vertical: 30),
+                            child: Column(
+                              children: [
+                                Text(
+                                  'Please read the instructions below before requesting help',
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                SizedBox(height: 20),
+                                Text(
+                                  'IMPORTANT: This emergency section should only be used in genuine emergency situations.',
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.red,
+                                  ),
+                                ),
+                                SizedBox(height: 20),
+                                Text(
+                                  'Please remember to activate your GPS to allow us to locate you easier',
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    color: Colors.black,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                          SizedBox(
+                            width: 200,
+                            child: DotLottieLoader.fromAsset(
+                              'assets/animations/alert.lottie',
+                              frameBuilder:
+                                  (BuildContext ctx, DotLottie? dotlottie) {
+                                if (dotlottie != null) {
+                                  return Lottie.memory(
+                                      dotlottie.animations.values.single);
+                                } else {
+                                  return Container();
+                                }
+                              },
+                            ),
+                          ),
+                          const SizedBox(height: 30),
+                          Container(
+                            constraints: const BoxConstraints(maxWidth: 300),
+                            child: DropdownButtonFormField<String>(
+                              decoration: const InputDecoration(
+                                labelText: 'Reason',
+                                border: OutlineInputBorder(),
+                              ),
+                              items: const [
+                                DropdownMenuItem(
+                                  value: 'General Accident',
+                                  child: Text('General Accident'),
+                                ),
+                                DropdownMenuItem(
+                                  value: 'Medical Emergency',
+                                  child: Text('Medical Emergency'),
+                                ),
+                                DropdownMenuItem(
+                                  value: 'Natural Disaster',
+                                  child: Text('Natural Disaster'),
+                                ),
+                                DropdownMenuItem(
+                                  value: 'Facility Damage',
+                                  child: Text('Facility Damage'),
+                                ),
+                              ],
+                              onChanged: (String? value) {
+                                viewModel.setReason(value);
+                              },
+                            ),
+                          ),
+                          const SizedBox(height: 40),
+                          SliderButton(
+                            action: _sendHelpRequest,
+                            label: const Text(
+                              'Slide to request help',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 14,
+                              ),
+                            ),
+                            icon: const Icon(
+                              Icons.keyboard_double_arrow_right,
+                              color: Colors.white,
+                              size: 50,
+                            ),
+                            buttonColor:
+                                lightColorScheme.tertiary.withOpacity(1),
+                            backgroundColor:
+                                lightColorScheme.tertiary.withOpacity(0.4),
+                            baseColor:
+                                lightColorScheme.secondary.withOpacity(0.7),
+                          ),
+                        ],
+                      ),
+                    ),
+                  if (viewModel.state == EmergencyState.isWaiting)
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 60, vertical: 60),
+                        child: Column(
+                          children: [
+                            const Text(
+                              'Please wait until an administrator can be reached',
+                              textAlign: TextAlign.center,
+                              style: TextStyle(fontSize: 20),
+                            ),
+                            const SizedBox(height: 130),
+                            Transform.scale(
+                              scale: 2,
+                              child: DotLottieLoader.fromAsset(
+                                  'assets/animations/rest-wait.lottie',
+                                  frameBuilder:
+                                      (BuildContext ctx, DotLottie? dotlottie) {
+                                if (dotlottie != null) {
+                                  return Lottie.memory(
+                                      dotlottie.animations.values.single);
+                                } else {
+                                  return Container();
+                                }
+                              }),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  if (viewModel.state == EmergencyState.isAdminApproved)
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 60, vertical: 60),
+                        child: Column(
+                          children: [
+                            const Text(
+                              'Congratulations, an administrator has approved your request and it\'s on its way',
+                              textAlign: TextAlign.center,
+                              style: TextStyle(fontSize: 20),
+                            ),
+                            const SizedBox(height: 100),
+                            Transform.scale(
+                              scale: 1.5,
+                              child: DotLottieLoader.fromAsset(
+                                  'assets/animations/meditation-wait-please.lottie',
+                                  frameBuilder:
+                                      (BuildContext ctx, DotLottie? dotlottie) {
+                                if (dotlottie != null) {
+                                  return Lottie.memory(
+                                    dotlottie.animations.values.single,
+                                  );
+                                } else {
+                                  return Container();
+                                }
+                              }),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                ],
+              ),
+            ),
+          );
+        }
       }),
     );
   }
