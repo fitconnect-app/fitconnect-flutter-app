ad06dabfe5683498c1c0ce36dff1529bbac14f4c
Juan Alegría <j.alegria@uniandes.edu.co>
2023-05-24 06:56:28 -0500

    Feat: create emergency dto

---
+++ lib/model/emergency/emergency_dto.dart
@@ -0,0 +1,64 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:fit_connect/model/emergency/emergency_model.dart';
+
+class EmergencyDTO {
+  final String id;
+  final String userName;
+  final String location;
+  final String reason;
+  final Timestamp timestamp;
+  final String status;
+
+  EmergencyDTO({
+    required this.id,
+    required this.userName,
+    required this.location,
+    required this.reason,
+    required this.timestamp,
+    required this.status,
+  });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'userName': userName,
+      'location': location,
+      'reason': reason,
+      'timestamp': timestamp,
+      'status': status,
+    };
+  }
+
+  factory EmergencyDTO.fromMap(DocumentSnapshot document) {
+    final data = document.data() as Map<String, dynamic>;
+    return EmergencyDTO(
+      id: document.id,
+      userName: data['userName'],
+      location: data['location'],
+      reason: data['reason'],
+      timestamp: data['timestamp'],
+      status: data['status'],
+    );
+  }
+
+  EmergencyModel toModel() {
+    return EmergencyModel(
+      id: id,
+      userName: userName,
+      location: location,
+      reason: reason,
+      timestamp: timestamp,
+      status: status,
+    );
+  }
+
+  factory EmergencyDTO.fromModel(EmergencyModel model) {
+    return EmergencyDTO(
+      id: model.id,
+      userName: model.userName,
+      location: model.location,
+      reason: model.reason,
+      timestamp: model.timestamp,
+      status: model.status,
+    );
+  }
+}
