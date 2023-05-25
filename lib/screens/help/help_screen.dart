b7bd9fb93a7da24a9f159da7d36e600fe5ca7832
Luis Castro <60227212+ldcastro10@users.noreply.github.com>
2023-05-24 23:04:22 -0500

    Fix: refactor help screen

--- lib/screens/help/help_screen.dart
+++ lib/screens/help/help_screen.dart
@@ -6,8 +6,6 @@ import 'package:motion_toast/motion_toast.dart';
 import 'package:motion_toast/resources/arrays.dart';
 import 'package:provider/provider.dart';
 import 'package:fit_connect/view_model/help_view_model.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'dart:async';
 
 class HelpScreen extends StatefulWidget {
   const HelpScreen({Key? key}) : super(key: key);
@@ -117,12 +115,6 @@ class HelpScreenState extends State<HelpScreen> {
                     },
                   ),
                   const SizedBox(height: 8),
-                  ElevatedButton(
-                    child: const Text("Go to Google Map"),
-                    onPressed: () {
-                      Navigator.of(context).push(MaterialPageRoute(builder: (context) => const MapSample()));
-                    },
-                  ),
                 ],
               ),
             );
@@ -184,50 +176,5 @@ class HelpScreenState extends State<HelpScreen> {
         bottomNavigationBar: const BottomNavBar(selectedTab: 2),
       ),
     );
-  }
-}
-
-class MapSample extends StatefulWidget {
-  const MapSample({Key? key}) : super(key: key);
-
-  @override
-  State<MapSample> createState() => MapSampleState();
-}
-
-class MapSampleState extends State<MapSample> {
-  final Completer<GoogleMapController> _controller = Completer<GoogleMapController>();
-
-  static const CameraPosition _kGooglePlex = CameraPosition(
-    target: LatLng(37.42796133580664, -122.085749655962),
-    zoom: 14.4746,
-  );
-
-  static const CameraPosition _kLake = CameraPosition(
-      bearing: 192.8334901395799,
-      target: LatLng(37.43296265331129, -122.08832357078792),
-      tilt: 59.440717697143555,
-      zoom: 19.151926040649414);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: GoogleMap(
-        mapType: MapType.hybrid,
-        initialCameraPosition: _kGooglePlex,
-        onMapCreated: (GoogleMapController controller) {
-          _controller.complete(controller);
-        },
-      ),
-      floatingActionButton: FloatingActionButton.extended(
-        onPressed: _goToTheLake,
-        label: const Text('To the lake!'),
-        icon: const Icon(Icons.directions_boat),
-      ),
-    );
-  }
-
-  Future<void> _goToTheLake() async {
-    final GoogleMapController controller = await _controller.future;
-    controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
   }
 }
