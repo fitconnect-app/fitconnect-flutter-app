6ab448cc2d04c01b9ce1dfb3d7aac84cb460981f
Juan Alegría <j.alegria@uniandes.edu.co>
2023-05-25 13:42:55 -0500

    Build: configure firebase hosting

--- admin-dashboard/svelte.config.js
+++ admin-dashboard/svelte.config.js
@@ -1,12 +1,24 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
+
+import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	// Consult https://github.com/sveltejs/svelte-preprocess
+	// for more information about preprocessors
+	preprocess: [
+		preprocess({
+			postcss: true,
+		}),
+	],
+
 	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter({
+			pages: 'public',
+			assets: 'public',
+			fallback: null,
+			precompress: false
+		}),
 	}
 };
 
