18ffe8f5b8694bc9b12b2024e09152fb73ae7ce0
ElReyZero <juan.romero1201@gmail.com>
2023-05-25 10:39:03 -0500

    Fix: Update regex in create event

--- lib/view_model/event_create_view_model.dart
+++ lib/view_model/event_create_view_model.dart
@@ -38,12 +38,12 @@ class EventCreateViewModel extends ChangeNotifier {
     createEventTrace.start();
     var uid = _auth.currentUser?.uid ?? '';
 
-    if (location.length > 50) {
+    if (location.length > 100) {
       _state = CreateState.error;
       notifyListeners();
       throw const FormatException(
-          "The event location cannot be longer than 50 characters");
-    } else if (RegExp(r'[^a-zA-Z0-9 \-\u00E1\u00E9\u00ED\u00F3\u00FA\u00F1]')
+          "The event location cannot be longer than 100 characters");
+    } else if (RegExp(r'[^a-zA-Z0-9 .#,\-\u00E1\u00E9\u00ED\u00F3\u00FA\u00F1]')
         .allMatches(location)
         .isNotEmpty) {
       _state = CreateState.error;
