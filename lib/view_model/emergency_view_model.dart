0e0ed62aa7cb0ad26db4c6528e4b9a990e3bf932
Juan Alegría <j.alegria@uniandes.edu.co>
2023-05-26 15:49:46 -0500

    Fix: prevent send messages through closed stream

--- lib/view_model/emergency_view_model.dart
+++ lib/view_model/emergency_view_model.dart
@@ -100,16 +100,17 @@ class EmergencyViewModel extends ChangeNotifier {
     final currentDateTime = DateTime.now();
     final timeDifference =
         currentDateTime.difference(emergency.timestamp.toDate());
+    final isRequestOutdated = timeDifference.inMinutes >= 30;
     // Show current last emergency approved request
-    if (timeDifference.inHours < 3 && emergency.status == 'APPROVED') {
+    if (!isRequestOutdated && emergency.status == 'APPROVED') {
       if (state == EmergencyState.isLoading) {
         changeEmergencyState(EmergencyState.isAdminApproved);
         notifyListeners();
       }
       return;
     }
-    // Delete approved and outdated last emergency request
-    if (timeDifference.inMinutes >= 30 || emergency.status == 'APPROVED') {
+    // Delete outdated last emergency request
+    if (isRequestOutdated) {
       _emergencyRepository.deleteEmergency(lastRequestId);
       _preferencesInstance.setString('lastEmergencyRequest', '');
       if (state == EmergencyState.isLoading) {
